a1d06e994179af51d68f69bdaf106b988e8afaec
Samrendra Barick <baricksamarendra@gmail.com>
2023-11-01 20:15:02 +0530

    Some changes in header file

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
        <Link to='/'>
-        <h1 className="font-bold text-sm sm:text-xl: flex flex-wrap">
-          <span className="text-slate-500">Samar</span>
+        <h1 className="font-bold text-base sm:text-lg lg:text-xl flex flex-wrap">
+          <span className="text-slate-500 ">Samar</span>
           <span className="text-slate-700">Estate</span>
         </h1>
         </Link>
