a1968961c8507c678e4d22cecb0c69b52e9f6960
Samrendra Barick <baricksamarendra@gmail.com>
2023-11-03 14:59:07 +0530

    Creating Sign-Up Page Functionality

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',// this is bcz to connect the fronend to backend
+        secure: false,//means when everytime api is called it will get back to our backend 3000 port to get and post data
+      },
+    },
+  },
   plugins: [react()],
 })
