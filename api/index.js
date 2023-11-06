50baadc057053e6d83b37412576c5cd9a1baa8e5
Samrendra Barick <baricksamarendra@gmail.com>
2023-11-06 23:48:38 +0530

    create update user Api Route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js"
+import cookieParser from 'cookie-parser';
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,6 +16,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("server is running on port 3000");
@@ -22,4 +24,15 @@ app.listen(3000, () => {
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
+
+//createing a middleware
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message ||'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+});
 
