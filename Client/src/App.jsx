ce4baad0272c4e0d92a66a02112a7f8fb8068be0
Samrendra Barick <baricksamarendra@gmail.com>
2023-11-09 13:13:35 +0530

    Complete Update User functionality

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,6 +8,7 @@ import About from "./Pages/About";
 import Header from "./Components/Header";
 import PrivateRoute from "./Components/PrivateRoute";
 import CreateListing from "./Pages/CreateListing";
+import UpdateListing from './Pages/UpdateListing';
 
 const App = () => {
   return(
@@ -21,6 +22,10 @@ const App = () => {
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
         </Route>
       </Routes>
     </BrowserRouter>
