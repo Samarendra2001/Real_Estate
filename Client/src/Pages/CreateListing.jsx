92d20b3138d26af68e694ea08fd7f0409e8c31aa
Samrendra Barick <baricksamarendra@gmail.com>
2023-11-10 10:45:00 +0530

    Completing listing page

--- Client/src/Pages/CreateListing.jsx
+++ Client/src/Pages/CreateListing.jsx
@@ -254,7 +254,9 @@ const CreateListing = () => {
             />
             <div className='flex flex-col items-center'>
               <p>Regular price</p>
-              <span className='text-xs'>($ / month)</span>
+              {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
             </div>
           </div>
           {formData.offer && (
@@ -270,7 +272,9 @@ const CreateListing = () => {
             />
             <div className='flex flex-col items-center'>
               <p>Discounted price</p>
-              <span className='text-xs'>($ / month)</span>
+              {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}
             </div>
           </div>
           )}
